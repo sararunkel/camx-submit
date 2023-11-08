144cca2b9bcd34841dc693a5c5c3f7327e96d619
Sara Runkel <sara.runkel@gwu.edu>
2023-11-08 18:23:54 +0000

    Current full run of CAMx.

--- camx_mpi_test_2.sh
+++ camx_mpi_test_2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # set output and error output
-#SBATCH --time 03:00:00
+#SBATCH --time 72:00:00
 #SBATCH -o /lustre/groups/anenberggrp/srunkel/camx/camx_runs/camx.mpi.nyc.%j.out
 #SBATCH -e /lustre/groups/anenberggrp/srunkel/camx/camx_runs/camx.mpi.nyc.%j.err
 
 # default queue, single node
-#SBATCH -p tiny -n 128
+#SBATCH -p defq -n 128
 # SBATCH --mail-user=sara.runkel@gwu.edu
 # SBATCH --mail-type=ALL
 
@@ -21,4 +21,4 @@ module load netcdf/4.6.1
 # Create a file which contains the list of cpu-nodes allocated by slurm for running this job.
 srun hostname > nodes
 
-csh CAMx_v7.20.36.12.20180502.ifort.noloop2.job
+csh CAMx_ifort.run.job
